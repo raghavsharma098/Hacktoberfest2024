3ca48b6c2bc17cba814437f6526df41f0f4adbf0
Raghav Sharma <68181453+raghavsharma098@users.noreply.github.com>
2024-10-05 16:23:48 +0530

    Create lenght.py

---
+++ lenght.py
@@ -0,0 +1,11 @@
+l1 = [10, 20, 30]
+n = len(l1)
+print("Length of l1 : ", n)
+
+l2 = []
+n = len(l2)
+print("Length of l2 : ", n)
+
+l3 = [[10, 20], ["gfg", "courses"], 10.5]
+n = len(l3)
+print("Length of l3: ", n)
